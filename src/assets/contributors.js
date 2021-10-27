f3c886a4513d067062b2bbc211ed719c54f34617
Rafael <rafaelsilvar74@gmail.com>
2021-10-27 18:42:14 -0300

    rafaelsilvar

--- src/assets/contributors.js
+++ src/assets/contributors.js
@@ -4,5 +4,11 @@ export const contributors = [
     githubUrl: 'https://github.com/patrickmonteiro',
     linkedinUrl: 'https://www.linkedin.com/in/engpatrickmonteiro/',
     githubImgProfile: 'https://avatars.githubusercontent.com/u/13258255?v=4'
+  },
+  {
+    name: 'Rafael Silva',
+    githubUrl: 'https://github.com/rafaeusilvar',
+    linkedinUrl: 'https://www.linkedin.com/in/rafael-silva-480a67222/',
+    githubImgProfile: 'https://avatars.githubusercontent.com/u/92217304?v=4'
   }
 ]
